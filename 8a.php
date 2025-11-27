289af8fe92d5936c0bf22450b3d401e98364020c
Sathwik Shetty <sathwik.23cs139@sode-edu.in>
2025-11-27 10:50:32 +0530

    Add files via upload

---
+++ 8a.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    body{
+        font-family:Arial ,sans-serif;
+        text-align: center;
+        margin-top: 50px;
+        
+    }
+    h1{
+        color:#333;
+    }
+     p{
+        font-size: 1.2em;
+
+     }
+     </style>
+</head>
+<body>
+   <?php
+   $file='counter.txt';
+   $count=file_exists($file)?(int)file_get_contents($file):0;(file_put_contents($file,++$count));
+?>
+<h1>Visitor content</h1>
+<p>Number of Visitors:<strong><?php echo $count;?></strong></p>
+   
+   </body>
+</html>
